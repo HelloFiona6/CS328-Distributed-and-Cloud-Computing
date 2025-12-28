1da0f7086ca0b9244afcbe67309cb7f9630a6c46
HelloFiona6 <1870341385@qq.com>
2025-12-28 21:38:38 +0800

    A3 Task0

--- assignment4/codebase/flask_app/app.py
+++ assignment4/codebase/flask_app/app.py
@@ -1,14 +1,47 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
+import signal
+import sys
+import threading
 
 app = Flask(__name__)
 
+shutting_down = False
 
 @app.route('/')
 def hello():
-  server_name = os.getenv('SERVER_NAME', 'Unknown Server')
-  return f'Hello from {server_name}!'
+    # server_name = os.getenv('SERVER_NAME', 'Unknown Server')
+    # return f'Hello from {server_name}!'
+    
+    if shutting_down:
+        return "Server shutting down", 503
+    
+    pod_name = os.getenv('HOSTNAME', 'unknown-pod')
+    pod_ip = os.getenv('POD_IP', 'unknown-ip')
+    node_name = os.getenv('NODE_NAME', 'unknown-node')
 
+    return (
+        f'Hello! This is server in pod "{pod_name}" '
+        f'(IP={pod_ip}) from node "{node_name}"!'
+    )
+
+@app.route('/chat/<username>')
+def chat(username):
+    institution = request.args.get('institution', 'unknown')
+    return jsonify({
+        "message": f"Hello {username} from {institution}!"
+    })
+
+def handle_sigterm(signum, frame):
+    # print("Received SIGTERM, shutting down gracefully...")
+    # sys.exit(0)
+    global shutting_down
+    print("SIGTERM received, stopping new requests...")
+    shutting_down = True
+    # allow in-flight requests to finish
+    threading.Timer(5, sys.exit).start()
+
+signal.signal(signal.SIGTERM, handle_sigterm)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=int(os.getenv('FLASK_PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.getenv('FLASK_PORT', 5000)))
